cfe931f88aa6feb16f6dc8c7a73ec7ca690d7a0d
matthewchiborak <matthewchiborak@hotmail.com>
2021-07-11 14:55:01 -0400

    Added api route to update characters stats that dont normally change through playing. Stuff like base stats, spells, hp, stuff like that so you can now level up or take new spells.

--- src/main/java/com/matthewchiborak/dndcharacterserver/controllers/CharacterController.java
+++ src/main/java/com/matthewchiborak/dndcharacterserver/controllers/CharacterController.java
@@ -48,10 +48,15 @@ public class CharacterController {
 		}
 	}
 	
+	@CrossOrigin
 	@PostMapping(value="/characters")
-	public String createCharacter(@RequestBody com.matthewchiborak.dndcharacterserver.model.Character character) {
+	public ResponseEntity<String> createCharacter(@RequestBody com.matthewchiborak.dndcharacterserver.model.Character character) {
 		com.matthewchiborak.dndcharacterserver.model.Character insertedCharacter = characterRepository.insert(character);
-		return "Character created " + insertedCharacter.getName();
+		
+		if(insertedCharacter != null)
+			return new ResponseEntity<>("{\"message\": \"" + insertedCharacter.getName() + " created successfully\"}", HttpStatus.OK);
+		else
+			return new ResponseEntity<>("{\"message\": \"Character Creation Failed\"}", HttpStatus.BAD_REQUEST);
 	}
 	
 	@CrossOrigin
@@ -80,6 +85,66 @@ public class CharacterController {
 			return null;
 		});
 		
-		return new ResponseEntity<>("{\"message\": \"" + newCharacter.getName() + " updated\"}", HttpStatus.OK);
+		return new ResponseEntity<>("{\"message\": \"" + newCharacter.getName() + " updated successfully\"}", HttpStatus.OK);
+	}
+	
+	@CrossOrigin
+	@PutMapping("/charactersstats")
+	public <U> ResponseEntity<String> updateCharacterStats(@RequestBody com.matthewchiborak.dndcharacterserver.model.Character newCharacter) {
+		
+		Optional<com.matthewchiborak.dndcharacterserver.model.Character> charData = characterRepository.findById(newCharacter.getId());
+		
+		if(!charData.isPresent())
+			return new ResponseEntity<>("{\"message\": \"Failed to update\"}", HttpStatus.NOT_FOUND);
+		
+		characterRepository.findById(newCharacter.getId()).map((Function<? super Character, ? extends U>) character -> {
+			character.setName(newCharacter.getName());
+			character.setSpeed(newCharacter.getSpeed());
+			character.setAc(newCharacter.getAc());
+			character.setProficiencyBonus(newCharacter.getProficiencyBonus());
+			character.setStrength(newCharacter.isStrength());
+			character.setConstitution(newCharacter.isConstitution());
+			character.setCharisma(newCharacter.isCharisma());
+			character.setDexterity(newCharacter.isDexterity());
+			character.setIntelligence(newCharacter.isIntelligence());
+			character.setWisdom(newCharacter.isWisdom());
+			character.setCurrentHP(newCharacter.getCurrentHP());
+			character.setMaxHP(newCharacter.getMaxHP());
+			character.setStrengthScore(newCharacter.getStrengthScore());
+			character.setConstitutionScore(newCharacter.getConstitutionScore());
+			character.setCharismaScore(newCharacter.getCharismaScore());
+			character.setDexterityScore(newCharacter.getDexterityScore());
+			character.setIntelligenceScore(newCharacter.getIntelligenceScore());
+			character.setWisdomScore(newCharacter.getWisdomScore());
+			character.setCurrentHitDie(newCharacter.getCurrentHitDie());
+			character.setMaxHitDie(newCharacter.getMaxHitDie());
+			character.setHitDieDice(newCharacter.getHitDieDice());
+			character.setCurrentSlots1(newCharacter.getCurrentSlots1());
+			character.setCurrentSlots2(newCharacter.getCurrentSlots2());
+			character.setCurrentSlots3(newCharacter.getCurrentSlots3());
+			character.setCurrentSlots4(newCharacter.getCurrentSlots4());
+			character.setCurrentSlots5(newCharacter.getCurrentSlots5());
+			character.setCurrentSlots6(newCharacter.getCurrentSlots6());
+			character.setCurrentSlots7(newCharacter.getCurrentSlots7());
+			character.setCurrentSlots8(newCharacter.getCurrentSlots8());
+			character.setCurrentSlots9(newCharacter.getCurrentSlots9());
+			character.setMaxSlots1(newCharacter.getMaxSlots1());
+			character.setMaxSlots2(newCharacter.getMaxSlots2());
+			character.setMaxSlots3(newCharacter.getMaxSlots3());
+			character.setMaxSlots4(newCharacter.getMaxSlots4());
+			character.setMaxSlots5(newCharacter.getMaxSlots5());
+			character.setMaxSlots6(newCharacter.getMaxSlots6());
+			character.setMaxSlots7(newCharacter.getMaxSlots7());
+			character.setMaxSlots8(newCharacter.getMaxSlots8());
+			character.setMaxSlots9(newCharacter.getMaxSlots9());
+			character.setSkills(newCharacter.getSkills());
+			character.setAttacks(newCharacter.getAttacks());
+			character.setCounters(newCharacter.getCounters());
+			character.setReferences(newCharacter.getReferences());
+			characterRepository.save(character);
+			return null;
+		}); 
+		
+		return new ResponseEntity<>("{\"message\": \"" + newCharacter.getName() + " updated successfully\"}", HttpStatus.OK);
 	}
 }
