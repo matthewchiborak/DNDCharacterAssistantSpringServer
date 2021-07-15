b98a95bc62aa71b7abd724a221f74a3a2701bea9
matthewchiborak <matthewchiborak@hotmail.com>
2021-07-15 14:02:14 -0400

    I changed the duration of the auth token to 24 hours.

--- src/main/java/com/matthewchiborak/dndcharacterserver/security/JwtUtil.java
+++ src/main/java/com/matthewchiborak/dndcharacterserver/security/JwtUtil.java
@@ -26,7 +26,7 @@ public class JwtUtil {
 								.map(GrantedAuthority::getAuthority)
 								.collect(Collectors.toList()))
 				.setIssuedAt(new Date(System.currentTimeMillis()))
-				.setExpiration(new Date(System.currentTimeMillis() + 600000))
+				.setExpiration(new Date(System.currentTimeMillis() + 1000*60*60*24))
 				.signWith(SignatureAlgorithm.HS512,
 						secretKey.getBytes()).compact();
 
